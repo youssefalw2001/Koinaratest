d746ae0918e11bde9644a5f8f5038574cb6cee65
youssefalw2001 <youssefalw2001@gmail.com>
2026-04-28 00:15:57 +0300

    Use compact pro Trade layout

--- artifacts/alpha-predict-pro/src/App.tsx
+++ artifacts/alpha-predict-pro/src/App.tsx
@@ -18,7 +18,7 @@ import { useQueryClient } from "@tanstack/react-query";
 import { LanguageProvider } from "@/lib/language";
 
 // Pages
-import Terminal from "./pages/TerminalReference";
+import Terminal from "./pages/TerminalCompact";
 import Mines from "./pages/Mines";
 import Earn from "./pages/Earn";
 import Shop from "./pages/Shop";
