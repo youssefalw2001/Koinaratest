f648e501725e2113b7756cb349b410b37ed3e731
youssefalw2001 <youssefalw2001@gmail.com>
2026-07-12 06:18:40 +0300

    test: add focused fast reflection pass

---
+++ gmgn_route_reflection_fast.py
@@ -0,0 +1,9 @@
+from __future__ import annotations
+
+import gmgn_route_reflection_map as scan
+
+scan.MAX_CASES = 120
+scan.WAIT_MS = 450
+
+if __name__ == "__main__":
+    raise SystemExit(scan.main())
