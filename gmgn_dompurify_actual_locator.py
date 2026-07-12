8c1a9df3e508e74e0493ed4a68882f429f273835
youssefalw2001 <youssefalw2001@gmail.com>
2026-07-12 05:35:52 +0300

    test: target inlined DOMPurify module

---
+++ gmgn_dompurify_actual_locator.py
@@ -0,0 +1,9 @@
+from __future__ import annotations
+
+import gmgn_dompurify_locator as locator
+
+# DOMPurify is bundled inline immediately before Html.tsx in module 461617.
+locator.TARGET_ID = "461617"
+
+if __name__ == "__main__":
+    raise SystemExit(locator.main())
